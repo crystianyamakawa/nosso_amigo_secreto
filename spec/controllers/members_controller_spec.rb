6d97feecba46fa03b0fb217726edb632d5ece32f
crystianyamakawa <crystian_yamakawa@hotmail.com>
2017-10-01 15:01:45 -0300

    controller member

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 RSpec.describe MembersController, type: :controller do
 
   describe "GET #create" do
-    it "returns http success" do
-      get :create
+    it "returns Json success" do
+      request.env["HTTP_ACCEPT"] = 'application/json'
+
+      @member = create(:member)
+        get :create
       expect(response).to have_http_status(:success)
     end
   end
+
 
   describe "GET #destroy" do
     it "returns http success" do
